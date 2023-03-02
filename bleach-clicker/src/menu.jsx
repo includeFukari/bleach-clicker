2f989a889fd1119d036875581b58e20abf82818a
includeFukari <tatiosey@protonmail.com>
2023-03-02 19:27:48 +0100

    push App.css push menu.jsx

--- bleach-clicker/src/menu.jsx
+++ bleach-clicker/src/menu.jsx
@@ -2,10 +2,11 @@ import { useState } from 'react'
 
 const Menu = () => {
     return (
-      <div>
+      <div id="MenuD">
         <h1 className="gameTitle">Bleach Clicker</h1>
         <h2 className="gameTitleUnder">A Bleach incremental game</h2>
       </div>
+      
     )
   }
 
