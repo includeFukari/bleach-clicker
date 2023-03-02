7792cda2eb505f4fc6c8ccf66f2066c14133f021
includeFukari <tatiosey@protonmail.com>
2023-03-02 17:57:28 +0100

    push

---
+++ bleach-clicker/src/menu.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+const Menu = () => {
+    return (
+      <div>
+        <h1 className="gameTitle">Bleach Clicker</h1>
+        <h2 className="gameTitleUnder">A Bleach incremental game</h2>
+      </div>
+    )
+  }
+
+export default Menu;
